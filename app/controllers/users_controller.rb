8de81c2069a3858c206c9a448807785a53951604
nik <nikenor11@gmail.com>
2019-06-13 11:41:55 +0300

    fix rubocop error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[index edit update destroy]
   before_action :correct_user, only: %i[edit update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user, only: :destroy
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url
   end
 
