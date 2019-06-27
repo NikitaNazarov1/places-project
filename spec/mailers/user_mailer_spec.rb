8e5f052aa7b9a8b058c362511806a64e1fd6e9da
nik <nikenor11@gmail.com>
2019-06-27 13:17:04 +0300

    fix rubocop & rspec failures

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,16 +4,25 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   describe 'account_activation' do
-    let(:mail) { UserMailer.account_activation }
+    let(:mail) { UserMailer.account_activation(user) }
+    let(:user) do
+      User.create(params)
+    end
+    let(:params) do
+      {
+
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john.doe@example.com',
+        password: '1234567',
+        password_confirmation: '1234567'
+      }
+    end
 
     it 'render the headers' do
       expect(mail.subject).to eq('Account activation')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
-    end
-
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['places.project.epam@gmail.com'])
     end
   end
 
@@ -23,7 +32,7 @@ RSpec.describe UserMailer, type: :mailer do
     it 'renders the headers' do
       expect(mail.subject).to eq('Password reset')
       expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
+      expect(mail.from).to eq(['places.project.epam@gmail.com'])
     end
 
     it 'renders the body' do
