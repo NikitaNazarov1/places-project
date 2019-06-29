21ae2f467664bb98433b06d113322480cbde69c7
nik <nikenor11@gmail.com>
2019-06-29 13:13:39 +0300

    add reset password

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -25,18 +25,4 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.from).to eq(['places.project.epam@gmail.com'])
     end
   end
-
-  describe 'password_reset' do
-    let(:mail) { UserMailer.password_reset }
-
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Password reset')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['places.project.epam@gmail.com'])
-    end
-
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
-    end
-  end
 end
